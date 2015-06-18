257c5fd78eb0cab5f1e098d1b6ebc380d40b7e3a
Harlan T Wood & Jack Senechal <code+harlan+jack@harlantwood.net>
2015-06-17 20:36:00 -0700

    stuff

---
+++ src/index.webgl.sphere.js
@@ -0,0 +1,47 @@
+'use strict';
+var famous = require('famous');
+
+var DOMElement = famous.domRenderables.DOMElement;
+var FamousEngine = famous.core.FamousEngine;
+var Node = famous.core.Node;
+var clock = FamousEngine.getClock();
+var Mesh = require('famous/webgl-renderables/Mesh')
+var Geometry = require('famous/webgl-geometries/Geometry')
+
+// Boilerplate
+FamousEngine.init();
+var scene = FamousEngine.createScene();
+var node = scene.addChild()
+//var element = new DOMElement(node)
+
+//new Geometry({ dynamic: true });
+
+// Nodes can contain both a DOM element and GL Mesh
+//var mesh = new Mesh(node);
+
+
+
+//mesh.setGeometry('Sphere');
+//mesh.setGeometry('Sphere', { detail: 100 });
+
+
+
+
+function CustomTriangle(options) {
+    Geometry.call(this,
+        { buffers: [
+            { name: 'pos', data: [-1, 1, 0, 0, -1, 0, 1, 1, 0], size: 3 },
+            { name: 'normals', data: [0, 0, 1, 0, 0, 1, 0, 0, 1], size: 3 },
+            { name: 'texCoord', data: [0.0, 0.0, 0.5, 1.0, 1.0, 0.0], size: 2 },
+            { name: 'indices', data: [0, 1, 2], size: 1 }
+        ]}
+    );
+}
+
+
+var geometry = new CustomTriangle();
+var mesh = new Mesh(node);
+mesh.setGeometry(geometry);
+
+
+
